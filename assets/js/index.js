b8c960218ee8e12004b8b7d4d2d122044af1878c
tayyabaomer <tayyabakhatoon321@gmail.com>
2025-01-18 09:26:46 -0800

    Created a other-icons file, design nav menu and sub menu

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+
+
+
+// Use this gsap tools when we use these utilities
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(Flip,ScrollTrigger,ScrollToPlugin,TextPlugin)
+    // gsap code here!
+
+
+
+
+
+   });
+
+
+// Nav Animation on hover
+const navLinks = document.querySelectorAll('nav > ul > li > a');
+const header = document.querySelector('header');
