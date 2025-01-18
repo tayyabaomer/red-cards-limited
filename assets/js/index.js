e3966739558f328a3aedde50e69700fc822d7632
tayyabaomer <tayyabakhatoon321@gmail.com>
2025-01-18 12:46:06 -0800

    navigation menu has done

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,6 +13,97 @@ document.addEventListener("DOMContentLoaded", (event) => {
    });
 
 
+
+// loading animation
+function timeLineLoading() {
+    const header = document.querySelector('header');
+
+    const tl = gsap.timeline()
+    
+    tl.from(header, {
+        y: 30,
+        opacity: 0,
+        duration: 1.1,
+        overflow: 'hidden'
+    })
+}
+
+
+
 // Nav Animation on hover
-const navLinks = document.querySelectorAll('nav > ul > li > a');
-const header = document.querySelector('header');
+function navAnimation() {
+
+    // animating sub menu on mouse enter
+    const hasSubmenu = document.querySelectorAll('.has-submenu');
+
+    hasSubmenu.forEach( (link) => { 
+    
+        const submenu = link.querySelector('ul');
+    
+        link.addEventListener('mouseenter', () => {
+            console.log("hello");
+                gsap.to(submenu, {
+                    // display: "block",
+                    visibility: "visible",
+                    duration: 0.5,
+                    opacity: 1,
+                    ease: "power2-out",
+                    y: 30
+                })
+    
+        })
+    
+        link.addEventListener('mouseleave', () => {
+            console.log("HI");
+            
+            gsap.to(submenu, {
+                // display: "none",
+                visibility: "hidden",
+                duration: 0.5,
+                opacity: 0,
+                ease: "power2-out",
+                y: -30
+            })
+        })
+    })
+    
+
+    // hover on anchor will let the box shdow appear
+        
+    const navLinks = document.querySelectorAll('header ul > li');
+
+    const header = document.querySelector('header');
+
+    navLinks.forEach(links => {
+        links.addEventListener('mouseenter', () => {
+            gsap.to(header, {
+                boxShadow: "0 0 20px #d4d4d433",
+                duration: 0.3
+
+            })
+        } )
+        links.addEventListener('mouseleave', () => {
+            gsap.to(header, {
+                boxShadow: "none",
+                duration: 0.3
+            })
+        } )
+    });
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// Loading animation of navbar with timeline function
+timeLineLoading()
+// Calling the nav function
+navAnimation()
