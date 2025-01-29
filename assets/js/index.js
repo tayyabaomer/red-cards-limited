f8f7a5a984b3f4d0a2c5bad453576b7808ae5ae6
tayyabaomer <tayyabakhatoon321@gmail.com>
2025-01-29 08:17:09 -0800

    started brands catalogue section

--- assets/js/index.js
+++ assets/js/index.js
@@ -225,55 +225,10 @@ function copyRightFooter() {
 }
 
 
-function cardsSlider() {
-    
-    const swiperEl = document.querySelector('swiper-container');
-    const swiper = swiperEl.swiper;
-    
-    var appendNumber = 6;
-    var prependNumber = 1;
-    document
-      .querySelector(".prepend-2-slides")
-      .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.prependSlide([
-          '<swiper-slide>Slide ' + --prependNumber + "</swiper-slide>",
-          '<swiper-slide>Slide ' + --prependNumber + "</swiper-slide>",
-        ]);
-      });
-    document
-      .querySelector(".prepend-slide")
-      .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.prependSlide(
-          '<swiper-slide>Slide ' + --prependNumber + "</swiper-slide>"
-        );
-      });
-    document
-      .querySelector(".append-slide")
-      .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.appendSlide(
-          '<swiper-slide>Slide ' + ++appendNumber + "</swiper-slide>"
-        );
-      });
-    document
-      .querySelector(".append-2-slides")
-      .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.appendSlide([
-          '<swiper-slide>Slide ' + ++appendNumber + "</swiper-slide>",
-          '<swiper-slide>Slide ' + ++appendNumber + "</swiper-slide>",
-        ]);
-      });
-}
 
 
 
 
-
-// Swiper JS function
-cardsSlider()
 // dynamic footer date
 copyRightFooter()
 // Svg grow and shrink
