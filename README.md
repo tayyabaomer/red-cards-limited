a01f520afe52b61c8b3fdaa271bc77b4b52283ea
tayyabaomer <tayyabakhatoon321@gmail.com>
2025-02-18 05:04:47 -0800

    Adjust responsiveness of about us page and change image extension avif to jpg of JAPAN ARTS and more

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # red-cards-limited
 This is our e-project of 1st 6 months semester end, The website is about cards shop who's name is "Red Cards Limiteed"  wants to bring their website live as they have many brands and these brands also have many events and occcasions cards category.
+
+
+# Here is our live website
+https://tayyabaomer.github.io/red-cards-limited/
--- assets/js/brands-data/detail-data.js
+++ assets/js/brands-data/detail-data.js
@@ -2670,8 +2670,220 @@ brandsData = [
     price: "$6.99",
     Stock: "Out of stock",
     image: "aprints-hajjmubarak4",
-  }
+  },
 
+
+
+  //EID MUBARAK CARD START JAPAN Arts 
+  {
+    id: "217",
+    title: "Eid Mubarak Joyful Celebration Card",
+    detail: "This elegant card captures the spirit of Eid with vibrant colors and intricate designs that symbolize happiness, peace, and togetherness. Perfect for sending blessings to loved ones, the card features a beautiful illustration of traditional Eid lanterns and crescent moons, adding to the festive atmosphere.",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: High-quality textured paper Design: Colorful Eid lanterns with gold foil accents Inside: Blank for personalized messagesPackaging: Comes with a matching envelope",
+    stock: "In Stock",
+    price: "rs:150",
+    image: "japan-eid1",
+
+  },
+  {
+    id: "218",
+    title: "Elegant Eid Mubarak Greeting Card",
+    detail: " For those who appreciate a touch of class, this card features a minimalist design with an elegant gold calligraphy of Eid Mubarak. The clean design with subtle floral patterns makes it a perfect choice for a more sophisticated expression of Eid wishes.",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 6 x 8 inches Material: Premium matte finish card stock Design: Gold foil calligraphy with embossed floral details Inside: Pre-written greeting with space for a personal messagePackaging: Comes with a cream-colored envelope",
+    stock: "In stock",
+    price: "Rs:140",
+    image: "japan-eid2",
+
+  },
+  {
+    id: "219",
+    title: "Traditional Eid Mubarak Card with Islamic Art",
+    detail: " This card celebrates the rich cultural heritage of Eid with intricate Islamic geometric patterns and a bold design of the mosque. The use of rich blue, gold, and silver colors creates a regal feeling, perfect for those who want to share the spiritual essence of Eid.",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Glossy finish paper with embossing Design: Islamic geometric patterns with mosque illustrationInside: Blank with a message of peace and joyPackaging: Includes a matching gold-foiled envelope",
+    stock: "Out of stock",
+    price: "Rs:120",
+    image: "japan-eid3",
+
+
+  },
+  {
+    id: "220",
+    title: "Family Bond Eid Mubarak Card",
+    detail: " This warm and cozy card highlights the importance of family and togetherness during Eid. Featuring an illustration of a family gathered around the table, sharing joy and love, this card is perfect for sending heartfelt wishes to family members and close friends.",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Glossy finish paper with embossing Design: Islamic geometric patterns with mosque illustrationInside: Blank with a message of peace and joyPackaging: Includes a matching gold-foiled envelope",
+    stock: "In stock",
+    price: "Rs:200",
+    image: "japan-eid4",
+
+  },
+  {
+    id: "221",
+    title: " Floral Eid Mubarak Card with Decorative Border",
+    detail: " This card brings beauty and nature to your Eid wishes with a stunning floral design, including roses and delicate vines along the borders. The warm and inviting hues make this card a lovely choice for sharing the happiness of Eid with anyone who appreciates floral elegance.",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Glossy finish paper with embossing Design: Islamic geometric patterns with mosque illustrationInside: Blank with a message of peace and joyPackaging: Includes a matching gold-foiled envelope",
+    stock: "In stock",
+    price: "Rs:170",
+    image: "japan-eid5",
+
+  },
+  {
+    id: "222",
+    title: "Minimalist Eid Mubarak Card with Silver Embellishment",
+    detail: " Simple yet sophisticated, this card features a clean and minimalist design with a silver-embellished Eid Mubarak greeting. The understated elegance makes it suitable for those who appreciate contemporary designs with a touch of refinement.",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Glossy finish paper with embossing Design: Islamic geometric patterns with mosque illustrationInside: Blank with a message of peace and joyPackaging: Includes a matching gold-foiled envelope",
+    stock: "Out of stock",
+    price: "Rs:160",
+    image: "japan-eid6",
+
+  },
+
+
+  //EID MUBARAK CARD END
+
+  //RAMADAN MUBARAK CARD START
+
+  {
+    id: "223",
+    title: " Ramadan Mubarak Crescent Moon Card",
+    detail: " This card features a glowing crescent moon, symbolizing the beginning of Ramadan. The soft gold accents on the moon and the intricate stars represent hope, purity, and new beginnings. A perfect choice for sending Ramadan blessings to friends and family..",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Premium matte cardstock Design: Crescent moon with gold foil details and star patterns Inside: Blank for personalized messagesPackaging: Comes with a matching envelope",
+    stock: "In stock",
+    price: "Rs:150",
+    image: "japan-ramdan1",
+
+  },
+  {
+    id: "224",
+    title: " Elegant Ramadan Mubarak Lantern Card",
+    detail: " This card showcases a beautifully detailed lantern, a symbol of light during the holy month. The lantern is surrounded by intricate Islamic patterns, creating a sense of spiritual beauty and warmth. Ideal for sharing heartfelt Ramadan wishes with loved ones.",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 6 x 8 inches Material: Smooth satin finish card stock Design: Elegant lantern with silver foil highlights and decorative Islamic motifs Inside: Pre-written Ramadan message with space for a personal notePackaging: Comes with a matching silver-embellished envelope",
+    stock: "In stock",
+    price: "Rs:170",
+    image: "japan-ramdan2",
+
+  },
+  {
+    id: "225",
+    title: "  Ramadan Mubarak Calligraphy Card",
+    detail: " This card features stunning Arabic calligraphy of Ramadan Mubarak in elegant gold and deep blue tones. The design is clean and modern, making it a great choice for those who appreciate traditional elements with a contemporary twist.",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Premium matte cardstock Design: Crescent moon with gold foil details and star patterns Inside: Blank for personalized messagesPackaging: Comes with a matching envelope",
+    stock: "Out Of stock",
+    price: "Rs:190",
+    image: "japan-ramdan3",
+
+  },
+  {
+    id: "226",
+    title: "  Peaceful Ramadan Mubarak Card",
+    detail: " This serene card features a peaceful mosque silhouette at sunset, representing tranquility and devotion during the holy month. Soft pastel colors and gentle details make it perfect for sending a message of peace and goodwill to family and friends..",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Premium matte cardstock Design: Crescent moon with gold foil details and star patterns Inside: Blank for personalized messagesPackaging: Comes with a matching envelope",
+    stock: "In stock",
+    price: "Rs:160",
+    image: "japan-ramdan4",
+
+  },
+  {
+    id: "227",
+    title: " Floral Ramadan Mubarak Card",
+    detail: " This card blends the beauty of nature with the spiritual essence of Ramadan. A bouquet of flowers is delicately illustrated, symbolizing growth, renewal, and blessings. The intricate design and soft colors make this card a thoughtful way to send Ramadan wishes",
+    category: "Eid-ul-fitr",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Premium matte cardstock Design: Crescent moon with gold foil details and star patterns Inside: Blank for personalized messagesPackaging: Comes with a matching envelope",
+    stock: "In stock",
+    price: "Rs:200",
+    image: "japan-ramdan5",
+
+  },
+
+  //RAMADAN MUBARAK CARD END
+
+  //HAJJ MUBARAK CARD START
+  {
+    id: "228",
+    title: " Hajj Mubarak Kaaba Blessings Card",
+    detail: "  This card features a stunning depiction of the Kaaba, the sacred heart of Islam, surrounded by pilgrims. The design embodies the reverence and spiritual significance of Hajj, capturing the moment of unity and devotion. Ideal for those wishing to convey heartfelt congratulations to someone who has completed the Hajj pilgrimage.",
+    category: "Hajjmubarak",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Premium glossy card stock Design: Kaaba illustration with pilgrims, golden accents, and a background of the Masjid al-Haram Inside: Blank for personalized messages Packaging: Comes with a matching envelope",
+    stock: "In stock",
+    price: "Rs:150",
+    image: "japan-hajj1",
+
+  },
+
+  {
+    id: "229",
+    title: " Elegant Hajj Mubarak Card with Arabic Calligraphy",
+    detail: " This card showcases beautiful Arabic calligraphy of Hajj Mubarak with intricate Islamic geometric designs. The use of gold foil enhances the luxurious feel, making it a perfect choice to celebrate the sacred journey of Hajj.",
+    category: "Hajjmubarak",
+    brands: "japan-art-press",
+    specification: "Size: 6 x 8 inches Material: Smooth satin finish for an elegant look Design: Arabic calligraphy with gold foil highlights and geometric patterns Inside: Pre-written Hajj greeting with room for a personal message Packaging: Includes a matching gold-foiled envelope",
+    stock: "In stock",
+    price: "Rs:160",
+    image: "japan-hajj2",
+
+  },
+
+  {
+    id: "230",
+    title: "Hajj Mubarak Pilgrimage Journey Card",
+    detail: "  This card is designed to celebrate the profound spiritual journey of Hajj. Featuring a symbolic illustration of the pilgrims walking around the Kaaba (Tawaf) and performing other rituals, it emphasizes the faith, unity, and devotion of those who undertake this sacred journey.",
+    category: "Hajjmubarak",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Premium glossy card stock Design: Kaaba illustration with pilgrims, golden accents, and a background of the Masjid al-Haram Inside: Blank for personalized messages Packaging: Comes with a matching envelope",
+    stock: "Out Of stock",
+    price: "Rs:180",
+    image: "japan-hajj3",
+
+  },
+
+  {
+    id: "231",
+    title: "Traditional Hajj Mubarak Card with Crescent Moon",
+    detail: " A card featuring the symbolic crescent moon and star, which hold deep significance in Islam, alongside the sacred imagery of the Kaaba. The traditional design celebrates the profound spiritual essence of Hajj while offering a timeless and respectful greeting.",
+    category: "Hajjmubarak",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Premium glossy card stock Design: Kaaba illustration with pilgrims, golden accents, and a background of the Masjid al-Haram Inside: Blank for personalized messages Packaging: Comes with a matching envelope",
+    stock: "In stock",
+    price: "Rs:180",
+    image: "japan-hajj4",
+
+  },
+
+  {
+    id: "232",
+    title: "  Hajj Mubarak Card with Golden Dome and Mosque",
+    detail: "  This card features a beautiful design of the golden dome of the Prophet’s Mosque (Al-Masjid an-Nabawi) in Medina. The majestic mosque and the detailed arches symbolize the holiness of the pilgrimage and its connection to the sacred cities of Mecca and Medina.",
+    category: "Hajjmubarak",
+    brands: "japan-art-press",
+    specification: "Size: 5 x 7 inches Material: Premium glossy card stock Design: Kaaba illustration with pilgrims, golden accents, and a background of the Masjid al-Haram Inside: Blank for personalized messages Packaging: Comes with a matching envelope",
+    stock: "In stock",
+    price: "Rs:200",
+    image: "japan-hajj5",
+
+  },
+
+  //HAJJ MUBARAK CARD END
 
 
 ]
